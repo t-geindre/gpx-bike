47d8273ebc9f302ac314d70992fb2c2264a8d142
Thierry geindre <github.com@nostalgeek.org>
2025-09-17 13:33:43 +0200

    Polsih

--- assets/panel.js
+++ assets/panel.js
@@ -12,24 +12,28 @@ toggle.addEventListener('click', () => {
 
 function addPanelEntry(trip, dist, gpxs) {
     trips.insertAdjacentHTML('beforeend', `
-        <div class="trip">
+        <div class="trip visible">
         <label>
-            <input type="checkbox" checked="checked">
+            <input type="checkbox" checked="checked" style="display: none">
             <strong>
                 <span style="background-color: ${trip.Color}"></span>
                 ${trip.Name}
             </strong><br />
-            ${formatDate(trip.Segments[0].Date)} - ${dist.toFixed(1)} km
+            ${formatDate(trip.Segments[0].Date)} - ${dist.toFixed(1)} km<br />
         </label>
         </div>
     `);
 
+    const row = trips.lastElementChild;
     const checkbox = trips.lastElementChild.querySelector('input');
+
     checkbox.addEventListener('change', () => {
         gpxs.forEach(gpx => {
             if (checkbox.checked) {
+                row.classList.add("visible");
                 map.addLayer(gpx);
             } else {
+                row.classList.remove("visible");
                 map.removeLayer(gpx);
             }
         })
